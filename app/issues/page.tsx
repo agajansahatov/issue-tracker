a196b86ffc84fa2d501959459738440c4970bcff
Agajan Sahatov <agajan.st@gmail.com>
2025-05-09 06:22:32 +0800

    Set up Radix UI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,14 @@
+"use client";
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-	return <div>IssuesPage</div>;
+	return (
+		<>
+			<div>IssuesPage</div>
+			<Button>New Issue</Button>
+		</>
+	);
 };
 
 export default IssuesPage;
