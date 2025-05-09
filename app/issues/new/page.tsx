9aeedb58f9fc2a42391b6f78f91f2bfb6c58dbf4
Agajan Sahatov <agajan.st@gmail.com>
2025-05-09 14:40:50 +0800

    Refactor: move submit form logic to a separate function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -33,6 +33,17 @@ export default function NewIssuePage() {
 	const [error, setError] = useState('');
 	const [isSubmitting, setSubmitting] = useState(false);
 
+	const onSubmit = handleSubmit(async (data) => {
+		try {
+			setSubmitting(true);
+			await axios.post('/api/issues', data);
+			router.push('/issues');
+		} catch (error) {
+			setSubmitting(false);
+			setError(`An unexpected error happened: ${error}`);
+		}
+	});
+
 	return (
 		<div className='max-w-xl'>
 			{error && (
@@ -40,18 +51,7 @@ export default function NewIssuePage() {
 					{error}
 				</Callout.Root>
 			)}
-			<form
-				onSubmit={handleSubmit(async (data) => {
-					try {
-						setSubmitting(true);
-						await axios.post('/api/issues', data);
-						router.push('/issues');
-					} catch (error) {
-						setSubmitting(false);
-						setError(`An unexpected error happened: ${error}`);
-					}
-				})}
-			>
+			<form onSubmit={onSubmit}>
 				<ul className='space-y-5'>
 					<li>
 						<TextField.Root placeholder='Title' {...register('title')} />
