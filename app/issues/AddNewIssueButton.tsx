9ca99cdf9735df30e9b5bb59de217487161f4b75
Agajan Sahatov <agajan.st@gmail.com>
2025-05-09 21:54:25 +0800

    Display the issues

---
+++ app/issues/AddNewIssueButton.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+
+const AddNewIssueButton = () => {
+	return (
+		<Button>
+			<Link href='/issues/new'>New Issue</Link>
+		</Button>
+	);
+};
+
+export default AddNewIssueButton;
