d2369074e4ec2bd0cf80743f4257b6bd322ad6b1
Agajan Sahatov <agajan.st@gmail.com>
2025-05-09 10:58:45 +0800

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,26 +1,34 @@
 'use client';
-
 import dynamic from 'next/dynamic';
 import React, { useState } from 'react';
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
 import axios, { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
 // Dynamically load SimpleMDE on client only
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
 	ssr: false,
 });
 
-interface IssueForm {
-	title: string;
-	description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 export default function NewIssuePage() {
 	const router = useRouter();
-	const { register, control, handleSubmit } = useForm<IssueForm>();
+
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IssueForm>({
+		resolver: zodResolver(createIssueSchema),
+	});
+
 	const [error, setError] = useState('');
 
 	return (
@@ -31,7 +39,7 @@ export default function NewIssuePage() {
 				</Callout.Root>
 			)}
 			<form
-				className='space-y-3'
+				className='space-y-5'
 				onSubmit={handleSubmit(async (data) => {
 					try {
 						await axios.post('/api/issues', data);
@@ -44,6 +52,11 @@ export default function NewIssuePage() {
 				})}
 			>
 				<TextField.Root placeholder='Title' {...register('title')} />
+				{errors.title && (
+					<Text color='red' as='p' className='pb-3'>
+						{errors.title.message}
+					</Text>
+				)}
 				<Controller
 					name='description'
 					control={control}
@@ -51,6 +64,11 @@ export default function NewIssuePage() {
 						<SimpleMDE placeholder='Description' {...field} />
 					)}
 				/>
+				{errors.description && (
+					<Text color='red' as='p' className='pb-3'>
+						{errors.description.message}
+					</Text>
+				)}
 
 				<Button className='!cursor-pointer'>Submit New Issue</Button>
 			</form>
